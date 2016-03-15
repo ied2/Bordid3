835efa3cb7ee2b6c88d493b452e42ab94b7d9829
ariarn92 <ariarn92@gmail.com>
2016-03-15 13:16:36 +0000

    Breytti myndum, skuggi á texta

--- app/src/main/java/is/aiga/bordid/HomeActivity.java
+++ app/src/main/java/is/aiga/bordid/HomeActivity.java
@@ -105,6 +105,7 @@ public class HomeActivity extends AppCompatActivity
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
+
         }
 
         return super.onOptionsItemSelected(item);
