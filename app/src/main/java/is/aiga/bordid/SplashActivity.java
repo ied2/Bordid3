0c7beecf4418be73c647785f389231dcdd0ba73c
ariarn92 <ariarn92@gmail.com>
2016-04-10 17:11:35 +0000

    Splash screen, finished

--- app/src/main/java/is/aiga/bordid/SplashActivity.java
+++ app/src/main/java/is/aiga/bordid/SplashActivity.java
@@ -3,9 +3,7 @@ package is.aiga.bordid;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import is.aiga.bordid.R;
-import is.aiga.bordid.HomeActivity;
-
+import android.os.Handler;
 
 
 public class SplashActivity extends Activity {
@@ -16,21 +14,20 @@ public class SplashActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        Thread timerThread = new Thread(){
-            public void run(){
-                try{
+        Thread timerThread = new Thread() {
+            public void run() {
+                try {
                     sleep(3000);
-                }catch(InterruptedException e){
+                } catch (InterruptedException e) {
                     e.printStackTrace();
-                }finally{
-                    Intent intent = new Intent(SplashActivity.this,HomeActivity.class);
+                } finally {
+                    Intent intent = new Intent(SplashActivity.this, HomeActivity.class);
                     startActivity(intent);
                 }
             }
         };
         timerThread.start();
     }
-
     @Override
     protected void onPause() {
         // TODO Auto-generated method stub
